ba90aad9017f5c499aff9d2080b97f8447d3be14
pskanade <k.pranav.apk@gmail.com>
2019-04-21 03:32:43 +0530

    Can enroll into a courses as per the canges

--- src/Actions/Enroll.js
+++ src/Actions/Enroll.js
@@ -1,28 +1,37 @@
 import Router from "next/router";
 
 const URLS = {
-  PATCH_COURSE_ENROLL: "http://127.0.0.1:8000/api/course"
+  PATCH_COURSE_ENROLL: "http://127.0.0.1:8000/api/course/enroll/"
 };
-
-// http://127.0.0.1:8000/api/course/10/enroll/
 
 const enrollEventHandler = async courseKey => {
   console.log("[Actions/Enroll.js] enroll in a course: ", courseKey);
-  const AnshdataToken = JSON.parse(localStorage.getItem("AnshdataUser"))[
-    "token"
-  ];
-  const URL = URLS.PATCH_COURSE_ENROLL + "/" + courseKey + "/enroll/";
-  console.log("URL", URL);
-  await fetch(URL, {
-    method: "PATCH",
-    headers: {
-      Authorization: `JWT ${AnshdataToken}`
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Enrolled into ", data);
-    });
+  try {
+    const AnshdataToken = JSON.parse(localStorage.getItem("AnshdataUser"))[
+      "token"
+    ];
+    const enrollmentData = {
+      course: String(courseKey)
+    };
+    // console.log("URL", URLS.PATCH_COURSE_ENROLL);
+    await fetch(URLS.PATCH_COURSE_ENROLL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${AnshdataToken}`
+      },
+      body: JSON.stringify(enrollmentData)
+    })
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        console.log("Enrolled into ", data);
+      });
+  } catch (err) {
+    console.log("[Enroll.js] Error when enrolling to a course : ", err);
+  }
 
   Router.push("/courses");
 };
