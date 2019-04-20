9da0613c32c0de94a7798f508f854f9f2b2ab441
pskanade <k.pranav.apk@gmail.com>
2019-04-20 20:14:02 +0530

    Can create course according to the backend requirement

--- src/Components/Plugins/Contrib/Course/Course.js
+++ src/Components/Plugins/Contrib/Course/Course.js
@@ -1,73 +1,204 @@
-import React, { useState } from "react";
-import { Container, Grid, Form } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Container, Grid, Form, Dropdown } from "semantic-ui-react";
 import Router from "next/router";
 
 const URLS = {
-  CREATE_COURSE: "http://127.0.0.1:8000/api/course/"
+  CREATE_COURSE: "http://127.0.0.1:8000/api/course/",
+  LIST_CATS: "http://127.0.0.1:8000/api/plat/cat/"
 };
 
-const createCourse = async (courseName, courseDesc) => {
-  console.log("[Course.js] Create Course clicked");
-  console.log("New Course Name", courseName);
-  console.log("New Course Desc", courseDesc);
-  const AnshdataToken = JSON.parse(localStorage.getItem("AnshdataUser"))[
-    "token"
-  ];
-  const courseData = {
-    title: courseName
+class CourseContribution extends Component {
+  state = {
+    title: "",
+    subject: "",
+    category: "",
+    isPublished: false,
+    creditPoints: 0,
+    description: ""
   };
-  const createCourseRes = await fetch(URLS.CREATE_COURSE, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `JWT ${AnshdataToken}`
-    },
-    body: JSON.stringify(courseData)
-  });
 
-  let newCourse = await createCourseRes.json();
-  console.log("Newly Created Course", newCourse);
-  Router.push("/courses");
-};
+  creditSelectionHandler = (event, { value }) => {
+    this.setState({ creditPoints: value });
+  };
 
-const course = props => {
-  const [courseName, setCourseName] = useState("");
-  const [courseDesc, setCourseDesc] = useState("");
+  categorySelectionHandler = (event, { value }) => {
+    this.setState({ category: value });
+  };
 
-  return (
-    <Container as="div" className={"ContribCoursePlugin"}>
-      <Grid>
-        <Grid.Row columns={3}>
-          <Grid.Column width="4" />
-          <Grid.Column width="8">
-            <h4>Create New Course</h4>
-            <Form onSubmit={() => createCourse(courseName, courseDesc)}>
-              <Form.Input
-                label="Course Title"
-                placeholder="Zero to 'HERO' !!"
-                value={courseName}
-                onChange={event => {
-                  setCourseName(event.target.value);
-                }}
-              />
-              <Form.TextArea
-                label="Course Description"
-                placeholder="Describe your course in short..."
-                value={courseDesc}
-                onChange={event => {
-                  setCourseDesc(event.target.value);
-                }}
-              />
-              <Form.Button type="submit" color="twitter">
-                Create Course
-              </Form.Button>
-            </Form>
-          </Grid.Column>
-          <Grid.Column width="4" />
-        </Grid.Row>
-      </Grid>
-    </Container>
-  );
-};
+  changeHandler = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    // console.log("[Course.js] onChangeHandler");
+    // console.log(name, value);
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
 
-export default course;
+  getNewCourseData = () => {
+    return {
+      title: this.state.title,
+      subject: this.state.subject,
+      category: this.state.category,
+      isPublished: this.state.isPublished,
+      creditPoints: this.state.creditPoints,
+      description: this.state.description
+    };
+  };
+
+  createCourse = async () => {
+    console.log("[Course.js] Create Course clicked");
+    const AnshdataToken = JSON.parse(localStorage.getItem("AnshdataUser"))[
+      "token"
+    ];
+    const courseData = this.getNewCourseData();
+    const createCourseRes = await fetch(URLS.CREATE_COURSE, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${AnshdataToken}`
+      },
+      body: JSON.stringify(courseData)
+    });
+    let newCourse = await createCourseRes.json();
+    console.log("Newly Created Course", newCourse);
+    Router.push("/courses");
+  };
+
+  renderCategoryChoise = () => {
+    let catOptions = [];
+    try {
+      catOptions = this.state.catList.map(cat => {
+        return {
+          id: cat.id,
+          text: cat.title,
+          value: cat.id
+        };
+      });
+      // console.log(catOptions);
+    } catch (err) {
+      console.log("did not pull up the cat list yet");
+    }
+
+    return (
+      <>
+        <br />
+        <Dropdown
+          placeholder="Select Category"
+          clearable
+          options={catOptions}
+          selection
+          onChange={this.categorySelectionHandler}
+        />
+        <br />
+        <br />
+      </>
+    );
+  };
+
+  renderCreditPointsChoise = () => {
+    const options = [
+      { key: 1, text: "1", value: 1 },
+      { key: 2, text: "2", value: 2 },
+      { key: 3, text: "3", value: 3 },
+      { key: 4, text: "4", value: 4 },
+      { key: 5, text: "5", value: 5 },
+      { key: 6, text: "6", value: 6 },
+      { key: 7, text: "7", value: 7 },
+      { key: 8, text: "8", value: 8 },
+      { key: 9, text: "9", value: 9 },
+      { key: 10, text: "10", value: 10 }
+    ];
+
+    return (
+      <>
+        <br />
+        <Dropdown
+          placeholder="Credit Points"
+          clearable
+          options={options}
+          selection
+          onChange={this.creditSelectionHandler}
+        />
+        <br />
+        <br />
+      </>
+    );
+  };
+
+  renderForm = () => {
+    return (
+      <>
+        <h4>Create New Course</h4>
+        <Form onSubmit={this.createCourse}>
+          {this.renderCategoryChoise()}
+          <Form.Input
+            label="Course Title"
+            placeholder="Zero to 'HERO' !!"
+            value={this.state.title}
+            name="title"
+            onChange={event => this.changeHandler(event)}
+          />
+          <Form.Input
+            label="Subject"
+            placeholder="Computer Science"
+            value={this.state.subject}
+            name="subject"
+            onChange={event => this.changeHandler(event)}
+          />
+          {this.renderCreditPointsChoise()}
+          <Form.TextArea
+            label="Course Description"
+            placeholder="Describe your course in short..."
+            value={this.state.description}
+            name="description"
+            onChange={event => this.changeHandler(event)}
+          />
+          <Form.Button type="submit" color="twitter">
+            Create Course
+          </Form.Button>
+        </Form>
+      </>
+    );
+  };
+
+  render() {
+    return (
+      <Container as="div" className={"ContribCoursePlugin"}>
+        <Grid>
+          <Grid.Row columns={3}>
+            <Grid.Column width="4" />
+            <Grid.Column width="8">{this.renderForm()}</Grid.Column>
+            <Grid.Column width="4" />
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
+  }
+
+  // The component management functions
+  getCategoryList = async () => {
+    console.log("[Course.js] get categories");
+    const AnshdataToken = JSON.parse(localStorage.getItem("AnshdataUser"))[
+      "token"
+    ];
+    await fetch(URLS.LIST_CATS, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${AnshdataToken}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => this.setState({ catList: data }));
+  };
+
+  componentDidMount() {
+    console.log("[Course.js] component did mount");
+    this.getCategoryList();
+  }
+}
+
+export default CourseContribution;
