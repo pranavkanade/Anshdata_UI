9dec436ebc8c955541d435dd88a9fc4e42c9b8a4
pskanade <k.pranav.apk@gmail.com>
2019-05-09 02:32:57 +0530

    1. Present detailed view of all the lessons and assignments in a module. 2. Added secondary menu to show all the course assignments at one spot.

--- src/Components/Plugins/Courses/Detailed.js
+++ src/Components/Plugins/Courses/Detailed.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import { getCourse } from "./requests";
 import Course from "../Contrib/Course/Render";
 import Module from "../Contrib/Module/Render";
-import {
-  Container,
-  Divider,
-  Grid,
-  Accordion,
-  Segment
-} from "semantic-ui-react";
+import Lesson from "../Contrib/Lesson/Render";
+import Assignment from "../Contrib/Assignment/Render";
+import { Container, Divider, Grid, Accordion, Menu } from "semantic-ui-react";
+
+const menuTypes = {
+  DETAIL: "Detailed",
+  ASSIGNMENT: "Assignments"
+};
 
 class DetailedCourse extends Component {
   state = {
     course: null,
-    activeModule: -1
+    activeModule: -1,
+    activeMenu: menuTypes.DETAIL
   };
 
   courseSaveHandler = course => {
@@ -31,6 +33,26 @@ class DetailedCourse extends Component {
       console.log("Module to expand: ", modId);
       this.setState({ activeModule: modId });
     }
+  };
+
+  renderAssignments = assignments => {
+    if (assignments === null) {
+      return null;
+    }
+
+    return assignments.map(assign => {
+      return <Assignment assignment={assign} type="detail" />;
+    });
+  };
+
+  renderLessons = lessons => {
+    if (lessons === null) {
+      return null;
+    }
+
+    return lessons.map(lsn => {
+      return <Lesson lesson={lsn} type="detail" />;
+    });
   };
 
   renderModules = () => {
@@ -56,11 +78,50 @@ class DetailedCourse extends Component {
           TODO: Edit this to put the rendered lessons and Assignments here
          */}
           <Accordion.Content active={mod.id === this.state.activeModule}>
-            <Segment>{"SizeForm"}</Segment>
+            <Grid>
+              <Grid.Row columns={2}>
+                <Grid.Column width={2} />
+                <Grid.Column width={14}>
+                  {this.renderLessons(mod.lessons)}
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={2}>
+                <Grid.Column width={1} />
+                <Grid.Column width={15}>
+                  {this.renderAssignments(mod.assignments)}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <br />
           </Accordion.Content>
         </Accordion>
       );
     });
+  };
+
+  renderSecondaryMenu = () => {
+    const { activeMenu } = this.state;
+    return (
+      <div>
+        <Menu size="large" pointing secondary widths={3} fluid color="violet">
+          <Menu.Item
+            name={menuTypes.DETAIL}
+            active={activeMenu === menuTypes.DETAIL}
+            onClick={() => {
+              this.setState({ activeMenu: menuTypes.DETAIL });
+            }}
+          />
+          <Menu.Item
+            name={menuTypes.ASSIGNMENT}
+            active={activeMenu === menuTypes.ASSIGNMENT}
+            onClick={() => {
+              this.setState({ activeMenu: menuTypes.ASSIGNMENT });
+            }}
+          />
+          <Menu.Item disabled />
+        </Menu>
+      </div>
+    );
   };
 
   render() {
@@ -72,10 +133,17 @@ class DetailedCourse extends Component {
         ) : null}
 
         <Divider />
+
+        {this.renderSecondaryMenu()}
         <Grid>
           <Grid.Row columns={3}>
-            <Grid.Column width={2} />
-            <Grid.Column width={13}>{this.renderModules()}</Grid.Column>
+            <Grid.Column width={1} />
+            <Grid.Column width={14}>
+              <br />
+              {this.state.activeMenu === menuTypes.DETAIL
+                ? this.renderModules()
+                : this.renderAssignments(this.state.course.assignments)}
+            </Grid.Column>
             <Grid.Column width={1} />
           </Grid.Row>
         </Grid>
