e172bee7cece7f23a45afa5a817777118ad715cb
pskanade <k.pranav.apk@gmail.com>
2019-05-09 21:45:43 +0530

    Added a route to reuse the detailed view of course

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const routes = require("next-routes")();
 
-routes.add("/", "/").add("/courses/:id", "/courses/detailed");
+routes
+  .add("/", "/")
+  .add("/courses/:id", "/courses/detailed")
+  .add("/courses/mod/:id", "/courses/detailed");
 
 module.exports = routes;
