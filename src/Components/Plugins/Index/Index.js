c5ebf3db71a77f305e429460bdaf6e681f900493
pskanade <k.pranav.apk@gmail.com>
2019-06-15 14:21:10 +0530

    Added how it works panel

--- src/Components/Plugins/Index/Index.js
+++ src/Components/Plugins/Index/Index.js
@@ -1,13 +1,21 @@
 import React from "react";
 
+import css from "./index.scss";
+
 import Hero from "./HeroCard/HeroCard";
+import WorkingBox from "./WorkingBox/WorkingBox";
 
 const index = props => {
   return (
     <div className={"IndexPlugin"}>
-      <Hero explore="/courses" contribute="/contribute" />
-      <br />
-      <hr />
+      <div className={css.landing}>
+        <Hero explore="/courses" contribute="/contribute" />
+        <img
+          className={css.backWorking}
+          src="../../../../../static/assets/back/howWorks.svg"
+        />
+        <WorkingBox />
+      </div>
     </div>
   );
 };
