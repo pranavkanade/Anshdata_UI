a1829ca9206400548d6330ae9e22788a3ff93c18
pskanade <k.pranav.apk@gmail.com>
2019-06-22 14:29:05 +0530

    Added a detailed card for draft courses

--- src/globals.js
+++ src/globals.js
@@ -2,3 +2,9 @@ export const courseListType = {
   CATALOG: "CATALOG",
   ENROLLED: "ENROLLED"
 };
+
+export const cardType = {
+  PUBLISHED: "PUBLISHED",
+  DRAFT: "DRAFT",
+  ENROLLED: "ENROLLED"
+};
