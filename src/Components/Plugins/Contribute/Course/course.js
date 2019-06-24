d3793cca62430fd7d4418257da7e8d1be28d02ec
pskanade <k.pranav.apk@gmail.com>
2019-06-24 08:46:09 +0530

    Added inactive sidebar with course creation form

--- src/Components/Plugins/Contribute/Course/course.js
+++ src/Components/Plugins/Contribute/Course/course.js
@@ -14,54 +14,10 @@ class CourseForm extends Component {
     });
   };
 
-  renderCategories = () => {
-    return (
-      <div class="menu">
-        <div class="item" data-value="jenny">
-          <img
-            class="ui mini avatar image"
-            src="/images/avatar/small/jenny.jpg"
-          />
-          Jenny Hess
-        </div>
-        <div class="item" data-value="elliot">
-          <img
-            class="ui mini avatar image"
-            src="/images/avatar/small/elliot.jpg"
-          />
-          Elliot Fu
-        </div>
-        <div class="item" data-value="stevie">
-          <img
-            class="ui mini avatar image"
-            src="/images/avatar/small/stevie.jpg"
-          />
-          Stevie Feliciano
-        </div>
-      </div>
-    );
-  };
-
   renderCourseForm = () => {
     return (
       <div className={css.courseForm}>
-        <input
-          className={css.inputTitle}
-          placeholder="Course Title"
-          name="courseName"
-          type="text"
-          value={this.state.courseName}
-          onChange={event => {}}
-        />
-        <div className={css.halfCut}>
-          <div class="ui fluid selection dropdown">
-            <input type="hidden" name="user" />
-            <i class="dropdown icon" />
-            <div class="default text">Select Friend</div>
-            {this.renderCategories()}
-          </div>
-          <div />
-        </div>
+        <Form onSaveHandler={this.courseSaveHandler} />
       </div>
     );
   };
@@ -72,10 +28,12 @@ class CourseForm extends Component {
         <div className={css.page}>
           <span className={css.heading}>Create New Course</span>
           <div className={css.courseCreationPlane}>
-            <div className={css.courseForm}>
-              <Form onSaveHandler={this.courseSaveHandler} />
+            {this.renderCourseForm()}
+            <div className={css.sidebar}>
+              <button>
+                <span>Save</span>
+              </button>
             </div>
-            <div className={css.sidebar} />
           </div>
         </div>
       </div>
