dc5d291bfc3e031831146d5d62e60284b59771e8
pskanade <k.pranav.apk@gmail.com>
2019-06-24 13:54:48 +0530

    Added new module card to use for drafts

--- src/Components/Generic/Cards/ModuleCard.js
+++ src/Components/Generic/Cards/ModuleCard.js
@@ -11,13 +11,20 @@ const renderLoader = () => {
   );
 };
 
-const renderStats = (lessons = 2, creditPoints = 10, assignments = 3) => {
+const renderAction = () => {
   return (
-    <div className={css.secBox}>
+    <div className={css.actionBar}>
       <button>
         <span className={css.label}>Start</span>
         <img src="../../../../static/assets/icon/arrow_forward_24px_outlined.svg" />
       </button>
+    </div>
+  );
+};
+
+const renderStats = (lessons = 2, creditPoints = 10, assignments = 3) => {
+  return (
+    <div className={css.secBox}>
       <div className={css.stat}>
         <span className={css.value}>{lessons}</span>
         <br />
@@ -54,11 +61,49 @@ export const ModuleCardMd = props => {
   return (
     <div className={css.mdModuleCard} onClick={() => props.select(module.id)}>
       <h3 className={css.title}>{module.title}</h3>
+      {renderAction()}
       {renderStats(
         module.lessons.length,
         getCreditPoints(module.assignments),
         module.assignments.length
       )}
+    </div>
+  );
+};
+
+const renderEditActionBar = () => {
+  return (
+    <div className={css.actionBar}>
+      <button className={css.delete}>
+        <span className={css.label}>Delete</span>
+        <img src="../../../../static/assets/icon/delete_sweep_24px_outlined.svg" />
+      </button>
+      <button className={css.edit}>
+        <span className={css.label}>Modify</span>
+        <img src="../../../../static/assets/icon/create_24px_outlined.svg" />
+      </button>
+    </div>
+  );
+};
+
+export const ModuleCardDraft = props => {
+  const module = props.module;
+  console.log("Module Card = ", module);
+
+  if (module === null || module.lessons === undefined) {
+    return <div className={css.mdModuleCard}>{renderLoader()}</div>;
+  }
+
+  return (
+    <div className={css.mdModuleCard} onClick={() => props.select(module.id)}>
+      <h3 className={css.title}>{module.title}</h3>
+      <p>{module.description}</p>
+      {renderStats(
+        module.lessons.length,
+        getCreditPoints(module.assignments),
+        module.assignments.length
+      )}
+      {renderEditActionBar()}
     </div>
   );
 };
