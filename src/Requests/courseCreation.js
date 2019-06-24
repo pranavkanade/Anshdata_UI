cda234c5d747e4c3533c0024f681b50efde7c281
pskanade <k.pranav.apk@gmail.com>
2019-06-24 16:39:22 +0530

    Can create new modules for a course

--- src/Requests/courseCreation.js
+++ src/Requests/courseCreation.js
@@ -1,6 +1,6 @@
 import { getAuthorization } from "./Authorization";
 
-export const createCourseHandler = async (courseData, courseId) => {
+export const createCourseHandler = async (courseData, courseId = null) => {
   console.log("[Course/Form.js] Create Course clicked : ", courseData);
   try {
     let mthd = "POST";
@@ -22,5 +22,33 @@ export const createCourseHandler = async (courseData, courseId) => {
     return newCourse.id;
   } catch (err) {
     console.log("[Course/Form.js] user is not logged in : ", err);
+  }
+};
+
+export const createModuleHandler = async (modData, modId = null) => {
+  console.log("[Module/Action.js] create new module: ", modData);
+  try {
+    let mthd = "POST";
+    let URL = `http://127.0.0.1:8000/api/course/mod/`;
+    if (modId !== null) {
+      mthd = "PATCH";
+      URL = `http://127.0.0.1:8000/api/course/mod/${modId}/`;
+    }
+
+    const moduleCreated = await fetch(URL, {
+      method: mthd,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getAuthorization()
+      },
+      body: JSON.stringify(modData)
+    });
+    let newModule = await moduleCreated.json();
+    // const page = window.location.pathname;
+    console.log("New Module : ", newModule);
+    // Router.push(page);
+    return newModule.course;
+  } catch (err) {
+    console.log("[Module/Action.js] Error when creating a module : ", err);
   }
 };
