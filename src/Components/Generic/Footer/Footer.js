0ab3979e07ba102af25b3874f067726865ec5fc2
pskanade <k.pranav.apk@gmail.com>
2019-06-29 15:36:44 +0530

    Added temporary footer

--- src/Components/Generic/Footer/Footer.js
+++ src/Components/Generic/Footer/Footer.js
@@ -6,10 +6,38 @@ import css from "./Footer.scss";
 const footer = props => {
   return (
     <div className={css.footer}>
-      <div className={css.brandLogo}>
-        <Link href="/">
-          <button>Anshdata</button>
-        </Link>
+      <div className={css.top}>
+        <div>
+          <span>About us</span>
+          <span>Blog</span>
+          <span>Forum</span>
+        </div>
+        <div>
+          <span>Support us</span>
+          <span>Contact</span>
+          <span>Contribute</span>
+        </div>
+        <div>
+          <span>Facebook</span>
+          <span>Twitter</span>
+          <span>Youtube</span>
+        </div>
+      </div>
+      <div className={css.bottom}>
+        <div className={css.brandLogo}>
+          <Link href="/">
+            <span>Anshdata</span>
+          </Link>
+        </div>
+
+        <div className={css.copywrite}>
+          <span>Copywrite &#9400; 2019 </span>
+          <span>Anshdata Inc.</span>
+        </div>
+
+        <div className={css.terms}>
+          <span>Terms</span>
+        </div>
       </div>
     </div>
   );
