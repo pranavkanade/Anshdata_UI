eee59d376614f0c62760d63e6d0de43e525b2a84
pskanade <k.pranav.apk@gmail.com>
2019-07-06 17:10:54 +0530

    Form of the cards corrected

--- src/Components/Generic/Cards/LessonCard.js
+++ src/Components/Generic/Cards/LessonCard.js
@@ -26,9 +26,6 @@ export default props => {
           <button className={css.delete}>
             <img src="../../../../static/assets/icon/delete_sweep_24px_outlined.svg" />
           </button>
-          <button className={css.fullscreen}>
-            <img src="../../../../static/assets/icon/fullscreen_24px_outlined_light.svg" />
-          </button>
         </div>
       </div>
     </div>
